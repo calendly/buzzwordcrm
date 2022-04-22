1d05c4479114872e788d1092a3c4501718787493
Tristan O'Neil <tristan.oneil@calendly.com>
2022-04-22 12:50:31 -0400

    Add React Router and migrate index page to React component

--- components/app.jsx
+++ components/app.jsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+import Login from './login.jsx';
 
-let App = () => <h1>Hello, world!</h1>;
+let App = () => (
+  <Routes>
+    <Route path="/" element={<Login />} />
+  </Routes>
+);
 
 window.onload = () => {
   const root = createRoot(document.getElementById('root'));
 
-  root.render(<App />);
+  root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 };
