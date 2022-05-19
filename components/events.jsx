0e840f95dcff87db7ecf36d62c82cd27479c9e24
Rasheeda Salaam <rasheeda.salaam@calendly.com>
2022-05-19 00:48:17 -0400

    allows filtering of events on paginated events page

--- components/events.jsx
+++ components/events.jsx
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'
+import Select from 'react-select';
+import { Link } from 'react-router-dom';
 
 export default () => {
   const [events, setEvents] = useState([]);
   const [pagination, setPagination] = useState({});
+  const [selectedOption, setSelectedOption] = useState('all-events');
+
+  const options = [
+    { value: 'all-events', label: 'All Events' },
+    { value: 'active-events', label: 'Active Events' },
+    { value: 'canceled-events', label: 'Canceled Events' },
+    { value: 'past-events', label: 'Past Events' },
+  ];
 
   const fetchData = async () => {
     const nextPageQueryParams = pagination.next_page
@@ -21,9 +30,19 @@ export default () => {
   useEffect(() => {
     fetchData();
   }, []);
+  
+  const currentDate = Date.now();
 
   return (
     <div className="container" style={{ marginTop: '50px' }}>
+      <div style={{ alignSelf: 'center', textAlign: 'center' }}>
+        <Select
+          defaultValue={selectedOption}
+          options={options}
+          placeholder="Choose Filter"
+          onChange={(event) => setSelectedOption(event.value)}
+        />
+      </div>
       <div className="row">
         <table className="striped centered">
           <thead>
@@ -37,10 +56,61 @@ export default () => {
           <tbody>
             {events.map((event) => (
               <tr key={event.uri}>
-                <td><Link to={`/events/${event.uri.split('/')[4]}`}>{event.name}</Link></td>
-                <td>{event.date}</td>
-                <td>{event.start_time}</td>
-                <td>{event.end_time}</td>
+                {selectedOption === 'all-events' && (
+                  <React.Fragment>
+                    <td>
+                      <Link to={`/events/${event.uri.split('/')[4]}`}>
+                        {event.name}
+                      </Link>
+                    </td>
+                    <td>{event.date}</td>
+                    <td>{event.start_time}</td>
+                    <td>{event.end_time}</td>
+                  </React.Fragment>
+                )}
+
+                {selectedOption === 'active-events' &&
+                  event.status === 'active' && (
+                    <React.Fragment>
+                      <td>
+                        <Link to={`/events/${event.uri.split('/')[4]}`}>
+                          {event.name}
+                        </Link>
+                      </td>
+                      <td>{event.date}</td>
+                      <td>{event.start_time}</td>
+                      <td>{event.end_time}</td>
+                    </React.Fragment>
+                  )}
+
+                {selectedOption === 'canceled-events' &&
+                  event.status === 'canceled' && (
+                    <React.Fragment>
+                      <td>
+                        <Link to={`/events/${event.uri.split('/')[4]}`}>
+                          {event.name}
+                        </Link>
+                      </td>
+                      <td>{event.date}</td>
+                      <td>{event.start_time}</td>
+                      <td>{event.end_time}</td>
+                    </React.Fragment>
+                  )}
+
+                {selectedOption === 'past-events' &&
+                  Date.parse(`${event.date}, ${event.start_time}`) <
+                    currentDate && (
+                    <React.Fragment>
+                      <td>
+                        <Link to={`/events/${event.uri.split('/')[4]}`}>
+                          {event.name}
+                        </Link>
+                      </td>
+                      <td>{event.date}</td>
+                      <td>{event.start_time}</td>
+                      <td>{event.end_time}</td>
+                    </React.Fragment>
+                  )}
               </tr>
             ))}
           </tbody>
